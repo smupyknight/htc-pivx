05292a7557aaf2006b0ed9d15460b745539c7a33
smupyknight <smupyknight@outlook.com>
2018-06-08 21:35:25 +0800

    Enable masternode

--- src/masternode-payments.cpp
+++ src/masternode-payments.cpp
@@ -264,7 +264,7 @@ void FillBlockPayee(CMutableTransaction& txNew, CAmount nFees, bool fProofOfStak
     CBlockIndex* pindexPrev = chainActive.Tip();
     if (!pindexPrev) return;
 
-    if (IsSporkActive(SPORK_13_ENABLE_SUPERBLOCKS)/* && budget.IsBudgetPaymentBlock(pindexPrev->nHeight + 1)*/) {
+    if (IsSporkActive(SPORK_13_ENABLE_SUPERBLOCKS) && budget.IsBudgetPaymentBlock(pindexPrev->nHeight + 1)) {
         budget.FillBlockPayee(txNew, nFees, fProofOfStake);
     } else {
         masternodePayments.FillBlockPayee(txNew, nFees, fProofOfStake);
